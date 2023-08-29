a115872c765157a8e3122e03a197e313d11449a4
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2023-08-29 10:51:18 -0400

    trying to get the functionality of the carts msth to function

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,14 @@
 let shopItemsData = [
   {
     id: "Ahgdfgj",
-    name: "Hipster Hat",
+    name: "Blue Hipster Hat",
     price:46,
-    desc: "Blue Hipster Hat with text on front.",
+    desc: "Blue Hipster Hat with text on front",
     img:"images/img-1.jpg",
+
+   
+   
+
 },
 {
     id: "SSASbmc",
@@ -12,19 +16,54 @@ let shopItemsData = [
     price:165,
     desc: "Day glo Orange Windbreaker jacket",
     img:"images/img-2.jpg",
+   
+
+    
+
 },
 {
     id: "LssPKkb",
     name: "Jogger Pant",
     price: 65,
-    desc: "Orange jogger pants",
+    desc: "Orange jogger pants, light and durable",
     img:"images/img-3.jpg",
+
+    
+
+
 },
 {
     id: "gOihgnq",
     name: "Trucker Hat",
     price:46,
-    desc: "Black trucker hat with upside down text in front.",
+    desc: "Black hipster hat with upside down text in front",
     img:"images/img-4.jpg",
+   
+   
+    
+
 },
-];
+{
+    id: "8jgnfAS",
+    name: "Trucker Hat",
+    price:46,
+    desc: "White and blue hipster hat, custom fonts available ",
+    img:"images/img-5.jpg",
+
+    
+    
+
+},
+{
+    id: "BHGTT7y",
+    name: "Hoodie",
+    price:120,
+    price:120,
+    desc: "Black Hoodie with Black text on front",
+    img:"images/img-6.jpg",
+
+   
+     },
+    ];
+
+ 
